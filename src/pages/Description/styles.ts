199105b65fce6aed185b0d9a116a76b93dcfaca2
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-18 08:14:33 -0300

    feat: component Description styles
    feat: component Description styles

---
+++ src/pages/Description/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
+
+export const SubTitle = styled.h3`
+  font-size: 30px;
+  color: #3a3a3a;
+`;
+
+export const Info = styled.div`
+  background: '#FF872C';
+  padding: 22px 32px;
+  border-radius: 5px;
+  color: '#fff';
+
+  h1,
+  h3 {
+    margin-top: 14px;
+    font-weight: normal;
+    line-height: 54px;
+  }
+`;
